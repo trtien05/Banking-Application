586c1727dd43252c3f84364dbfd3a9646ea4fa5b
trtien05 <trantien100700@gmail.com>
2024-07-29 12:25:04 +0700

    finish right-sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBanlanceBox from '@/components/TotalBanlanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {
+    firstName: 'Adrian',
+    lastName: 'JSM',
+    email: 'contact@jsmastery.pro'
+  }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and trasactions efficiently."
+          />
+          <TotalBanlanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={12550.50}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+
+      />
+
+    </section>
   )
 }
 

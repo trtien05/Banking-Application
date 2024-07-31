09cdfa2ee3e05e5768b3c26e9302a92f41a52a6b
trtien05 <trantien100700@gmail.com>
2024-07-31 21:11:44 +0700

    fix payment transfer

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,8 @@
 import HeaderBox from '@/components/HeaderBox'
-import React from 'react'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
 import { getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
+import React from 'react'
 
 const Transfer = async () => {
   const loggedIn = await getLoggedInUser();
@@ -17,8 +17,8 @@ const Transfer = async () => {
   return (
     <section className="payment-transfer">
       <HeaderBox
-        title='Payment Transfer'
-        subtext='Please provide any specific details or notes related to the payment transfer'
+        title="Payment Transfer"
+        subtext="Please provide any specific details or notes related to the payment transfer"
       />
 
       <section className="size-full pt-5">
@@ -28,4 +28,4 @@ const Transfer = async () => {
   )
 }
 
-export default Transfer 
+export default Transfer
